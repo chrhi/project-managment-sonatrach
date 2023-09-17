a7253019980f28ec34282c0394fc9d1dc99d43bb
abdellah <Abdellah.chehri14@gmail.com>
2023-09-17 09:54:32 +0200

    add

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,3 +5,21 @@ export const config = { matcher: ["/private/:path*"] };
 // export default function middleware() {
 //   //todo handle that later
 // }
+
+// import { withAuth } from "next-auth/middleware"
+
+// // More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
+// export default withAuth({
+//   callbacks: {
+//     authorized({ req, token }) {
+//       // `/admin` requires admin role
+//       if (req.nextUrl.pathname === "/admin") {
+//         return token?.userRole === "admin"
+//       }
+//       // `/me` only requires the user to be logged in
+//       return !!token
+//     },
+//   },
+// })
+
+// export const config = { matcher: ["/admin", "/me"] }
